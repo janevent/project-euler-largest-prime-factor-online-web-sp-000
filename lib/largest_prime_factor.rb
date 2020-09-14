83efa356e2d0fa89ff3d6dbfdcf891554e5f57d8
Jane Venter <janeanneventer@gmail.com>
2020-09-14 13:32:57 +0000

    Automatically backed up by Learn

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+  array = []
+  p = 2 
+  while p < number-1
+    if number%p == 0 
+      i = 2 
+      while i < p-1 
+        if p % i == 0 
+          break
+        else 
+          array << p 
+        end
+      end
+      #array << p
+    end
+    p +=1
+  end
+  puts array
+end
