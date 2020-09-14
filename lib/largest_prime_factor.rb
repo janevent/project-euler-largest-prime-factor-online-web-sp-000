f683ffe879d7a4c5c9b42d2365e48e9e6feb1a61
Jane Venter <janeanneventer@gmail.com>
2020-09-14 13:39:43 +0000

    Automatically backed up by Learn

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,20 +1,27 @@
 # Enter your procedural solution here!
-def largest_prime_factor(number)
-  array = []
-  p = 2 
-  while p < number-1
-    if number%p == 0 
-      i = 2 
-      while i < p-1 
-        if p % i == 0 
-          break
-        else 
-          array << p 
-        end
-      end
-      #array << p
-    end
-    p +=1
+def largest_prime_factor(n)
+  factors = []
+  (2..n-1).each do |num|
+    if n%num ==0 && Prime.prime?(num)
+      factors << num
   end
-  puts array
 end
+# def largest_prime_factor(number)
+#   array = []
+#   p = 2 
+#   while p < number-1
+#     if number%p == 0 
+#       i = 2 
+#       while i < p-1 
+#         if p % i == 0 
+#           break
+#         else 
+#           array << p 
+#         end
+#       end
+#       #array << p
+#     end
+#     p +=1
+#   end
+#   puts array
+# end
