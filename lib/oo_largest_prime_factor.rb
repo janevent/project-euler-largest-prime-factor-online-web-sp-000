5003bd464054f9c9f22aa79a6a8c001f416611b3
Jane Venter <janeanneventer@gmail.com>
2020-09-14 14:38:10 +0000

    Automatically backed up by Learn

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -2,6 +2,33 @@
 class LargestPrimeFactor 
   attr_accessor :number 
   
-  def initialize 
+  def initialize(n)
+    @n = n
+  end
+  
+  def is_prime?(digit)
+    array = []
+    (2..digit-1).each do |d|
+      if digit% d == 0 
+        array << d 
+      end
+    end
+    return true if array.empty? 
+    false
+  end
+  
+  def largest_prime 
+    i = 3 
+    result = 0
+    ni = self.n
+    while i <= ni 
+      if ni % i == && is_prime?(i) 
+        result = i 
+        ni = ni/i 
+        
+      end
+     i +=1
+    end
+    @number = result
   end
 end
